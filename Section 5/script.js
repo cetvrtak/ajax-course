cf58b419041d8d70bfb4e64ced6a9ae338711b07
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-10-25 10:04:50 +0200

    Section 5 - pt 5

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,79 +1,38 @@
 const btn = document.querySelector(".btn");
 const inpEle = document.querySelector("input");
 const output = document.querySelector(".output");
+const container = document.querySelector(".container");
+inpEle.style.display = "none";
+btn.textContent = "Load GitHub JSON data";
 
-const myForm = document.createElement("form");
-document.body.append(myForm);
-
-const output1 = document.createElement("div");
-output1.classList.add("main");
-const baseUrl =
-  "https://script.google.com/macros/s/AKfycbzUcUVn99AkTK1rxxjCd-oU_707N3s23p9OriMaMzCYunuacydj/exec";
-inpEle.classList.add("box");
-inpEle.setAttribute("name", "nameOG");
-inpEle.value = "Hello World";
-myForm.append(inpEle);
-for (let i = 0; i < 10; i++) {
-  const myInput = document.createElement("input");
-  myInput.setAttribute("type", "text");
-  myInput.setAttribute("placeholder", "Value " + i);
-  myInput.classList.add("box");
-  myInput.setAttribute("name", "name" + (i + 1));
-  myInput.value = "Value " + i;
-  myForm.append(myInput);
-}
-myForm.append(btn);
-output.append(output1);
-btn.classList.add("box");
-
-btn.addEventListener("click", getPost);
+const baseurl = "https://my-json-server.typicode.com/cetvrtak/json-file/posts/";
+btn.addEventListener("click", loadData);
 
 function loadData(e) {
-  e.preventDefault();
   console.log("ready");
-  let formData = new FormData(myForm);
-  let data = [...formData.entries()];
-  console.log(data);
-  const params = data
-    .map((x) => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
-    .join("&");
-  console.log(params);
-  let url = baseUrl + "?" + params;
-  getData(url);
-}
-
-function getPost(e) {
-  e.preventDefault();
-  const headers = new Headers();
-  let formData = new FormData(myForm);
-  let body = {};
-  formData.forEach((val, key) => {
-    body[key] = val;
-  });
-  console.log(body);
-
-  const options = {
-    method: "POST",
-    headers,
-    body: JSON.stringify(body),
-  };
-  fetch(baseUrl, options)
-    .then((res) => res.json())
-    .then((data) => outputObj(data));
-}
-
-function getData(url) {
+  const url = baseurl;
   fetch(url)
-    .then((res) => res.json())
+    .then((rep) => rep.json())
     .then((data) => {
-      outputObj(data);
+      createPage(data);
     });
 }
 
-function outputObj(obj) {
-  console.log(obj);
-  output1.innerHTML = "";
-  for (const prop in obj) {
-    output1.innerHTML += `${prop} : ${obj[prop]}<br>`;
-  }
+function createPage(data) {
+  console.log(data);
+  output.innerHTML = "";
+  data.forEach((el) => {
+    const main = addEle(output, "div", "");
+    addEle(main, "div", el.title);
+    addEle(main, "div", el.body);
+    addEle(main, "div", el.id);
+    main.classList.add("box");
+  });
+}
+
+function addEle(parent, t, html) {
+  const ele = document.createElement(t);
+  parent.append(ele);
+  ele.innerHTML = html;
+  return ele;
 }
